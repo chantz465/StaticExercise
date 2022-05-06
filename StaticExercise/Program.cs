7af6429bf4addffab588678cf22a0f7a439b4ed7
chantz465 <chantz465@gmail.com>
2022-05-06 08:43:44 -0400

    (first add)

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(23); 
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(90); 
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celsius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
